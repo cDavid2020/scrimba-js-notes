f7e3e47c37262042f2b118802927d2eaa743c517
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2022-12-30 21:50:42 -0600

    add scrimba js lessons
    add scrimba js lessons

---
+++ Variables.js
@@ -0,0 +1,55 @@
+lef firstName = "Dylan";
+firstName = "Zack";
+
+let lastName = "Israel";
+let fullName = lastName + "," + firstName;
+
+let age = 32;
+let isEmployed = false;
+isEmploye = true;
+
+
+
+Israel, Zack
+
+
+console.log(fullName);
+console.log("Miyagi");
+console.log("Barney");
+console.log("tmnt");
+
+
+let firstName = "Michael";
+
+
+/*  */
+let name = "Gator";
+let age = 1;
+age = age +1;   
+
+console.log('My dog ' + name + ' is turning ' + age + 
+
+
+
+/* const variables */
+const = Constant
+
+const buildDate = "Oct, 26, 2020";
+
+let releasedDate;
+releasedDate = "oct, 29, 2020";
+
+
+
+
+/* const challenge */
+const name = "Gator";
+let age = 1;
+age = age + 1;
+
+console.log('My dog ' + name + ' is turning ' + age + '.');
+
+My dog Gator is turning 2.
+
+
+
---
+++ function-that-logs-sum.js
@@ -0,0 +1,16 @@
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3 //method 1
+    console.log(totalTime)//method 2 both prints same in console
+}
+
+
+
+
+
+
+logLapTime
